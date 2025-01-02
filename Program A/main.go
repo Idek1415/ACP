c5b05d08ae150ef6a8558ba3d3d9060f5e30852a
Idek5803 <yeshedyodsampa@icloud.com>
2025-01-02 23:20:50 +0000

    Program A     print("Hello World") complete

---
+++ Program A/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func test(){
+	fmt.Println("Hello World")
+}
+
+func main(){
+	//fmt.Println("Hello World")
+	test()
+}
+
+
+//input "go run "Program A"/main.go" into terminal to execute code
