74bfda274fb49f7a2c83b0c8d39ca709482fe1b1
Idek5803 <yeshedyodsampa@icloud.com>
2025-01-03 00:42:14 +0000

    Program B INCOMPLETE

---
+++ Program B/main.go
@@ -0,0 +1,119 @@
+//		Input "go run "Program B"/main.go" into terminal to run file
+
+/*
+Demonstrate loops,  DONE
+arrays,	DONE
+slices, MAYBE MORE
+math operations, DONE
+conditional statements, DONE
+functions/methods, 
+user input/output
+*/
+
+
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"math"
+)
+
+func playWithLoops(){
+	fmt.Println("Loop counts to 4")
+	for i := 1; i < 5; i ++{
+		fmt.Print(i)
+		if (i < 4) {
+			fmt.Print(", ")
+		} 
+	}
+	fmt.Println()
+}
+
+func playWithArrays(){
+	array1 := [4]int{}
+	array2 := [4]int{1,2,3,4}
+	strArray := [4]string{"Duck", "Duck", "Duck", "Goose"}
+
+	fmt.Println(array1)
+	fmt.Println(array2)
+	fmt.Println(strArray)
+
+	fmt.Println("Changed an indice in an array:")
+	array1[2] = 2
+	fmt.Println(array1)
+
+	fmt.Println("Length of that Array: " + strconv.Itoa(len(array1)))
+
+	println("Loop through a different array")
+	for /*leave as _, if you don't want to enumerate*/index, value := range array2{
+		fmt.Println("Index ", index, ": ", value)
+	}
+
+}
+
+func playWithSclices(){
+	array1 := [4]int{1,2,3,4}
+	slice1 := array1[:]
+	fmt.Println(slice1)
+
+	fmt.Println("append the int '5' to a slice")
+	slice1 = append(slice1, 5)
+	fmt.Println(slice1)
+}
+
+func playWithMath(){
+	num := 2
+
+	fmt.Println("Base number (BN): " , num)
+	fmt.Println(num, " plus 10: ", (num + 10))
+	fmt.Println(num, " minus 10: ", (num - 10))
+	fmt.Println(num, " times 10: ", (num * 10))
+	fmt.Println(num, " divided by 10: ", (num / 10))
+	fmt.Println(num, " divided by 10 (as a float): ", (float64(num)/10)) //		NOTE: simply deviding by 10.0 does not work
+
+	fmt.Println(num, " to the power of 10: ", (math.Pow(float64(num), 10)))
+	fmt.Println("Log base 10 of ", num, ": ", (math.Log10(float64(num))))
+	fmt.Println("Square root of ", num, ": ", (math.Sqrt(float64(num))))
+	fmt.Println("Sin(", num, "): ", math.Sin(float64(num)), " (Radians)")
+}
+
+func playWithConditionals(){
+	num := 2.0
+
+	fmt.Println(num, " is equal to 2.0: ", (num == 2.0))
+	fmt.Println(num, " is NOT equal to 2.0: ", (num != 2.0))
+	fmt.Println(num, " is less than 2.0: ", (num < 2.0))
+	fmt.Println(num, " is greater than 2.0: ", (num > 2.0))
+	fmt.Println(num, " is a multiple of 2.0: ", (int64(num) % 2.0 == 0))
+}
+
+func playWithMethods(){
+
+	type Thing interface {
+		DoSomething()
+	}
+	type Thing struct {
+		thingNumber int
+		thingSkill string
+	}
+	
+	func (t Thing) DoSomething() {
+		fmt.Println("I am Thing ", t.thingNumber, " and I am good at ", t.thingSkill)
+	}
+
+	T := Thing{thingNumber: 0, thingSkill: "nothing"}
+	T.DoSomething()
+}
+
+
+func main(){
+	fmt.Println("Program B")
+	//playWithLoops()
+	//playWithArrays()
+	//playWithSclices()
+	//playWithMath()
+	//playWithConditionals()
+	playWithMethods()
+
+}
